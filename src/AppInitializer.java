40051f55b9ede83b9aef03e9a87d21ac981faa7a
ozen-hub <info.ozenhub@gmail.com>
2023-04-07 21:29:10 +0530

    1

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,5 +1,19 @@
+import java.sql.SQLException;
+import java.util.Date;
+
 public class AppInitializer {
     public static void main(String[] args) {
+        CrudManager crudManager = new CrudManager();
+        Account account = new Account("A-1","Nimal",25000);
+        Payment payment = new Payment(
+                "P-1",new Date(),2500);
+        try{
+            System.out.println(
+                    crudManager.saveData(account,payment)
+            );
+        }catch (SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
 
     }
 }
